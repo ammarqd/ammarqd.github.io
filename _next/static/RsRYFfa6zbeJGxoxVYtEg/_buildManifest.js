fe123825791acf7360d3e3900942d7e7baf88b7c
ammarqd <ammarqd@outlook.com>
2025-10-27 16:03:01 +0000

    Updates

--- _next/static/RsRYFfa6zbeJGxoxVYtEg/_buildManifest.js
+++ _next/static/RsRYFfa6zbeJGxoxVYtEg/_buildManifest.js
@@ -1,12 +1,7 @@
 self.__BUILD_MANIFEST = {
   "__rewrites": {
     "afterFiles": [],
-    "beforeFiles": [
-      {
-        "source": "/new-portfolio//_next/:path+",
-        "destination": "/new-portfolio/_next/:path+"
-      }
-    ],
+    "beforeFiles": [],
     "fallback": []
   },
   "sortedPages": [
